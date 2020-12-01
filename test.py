660c4ced7f3eae0df9056f4bfeb4ac482813d0fd
Afes309 <66091434+Afes309@users.noreply.github.com>
2020-12-01 15:49:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
+from matplotlib.dates import datetime as dt
+import time
 
 
+time.ctime
 
 fig, ax = plt.subplots()
+
+by_price = 19530
 
 
 def test():
@@ -14,32 +19,41 @@ def test():
 	r = requests.post('https://api.exmo.com/v1.1/trades', headers = headers, data = data)
 	
 
-	#now_price = r.json()['BTC_USD'][0]['price']
+	now_price = float(r.json()['BTC_USD'][0]['price'])
+	now_date = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(r.json()['BTC_USD'][0]['date']+10800))
+	date = []
+	price = []
 
-	#for i in r.json()['BTC_USD']:
-		#print(i)
+	for i in r.json()['BTC_USD']:
+		price.append(float(i['price']))
+		date.append(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(i['date']+10800)))
 
-	print(r.text)
+	#print(date)
+	return price,date,now_price,now_date
 
 
-y = [18025,18090,18200,18100,19000,19200,18990,18700]
-x = [0,1,2,3,4,5,6,7]
+price,date_s,now_price,now_date_in = test()
 
-y1 = 19200
-x1 = 5
+date = [dt.datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in date_s]
 
-ax.plot(x,y)
-ax.scatter(x1,y1)
-ax.hlines(y1*0.99, x[0],x[-1] )
-ax.hlines(y1*1.01, x[0],x[-1] )
+now_date = dt.datetime.strptime(now_date_in, '%Y-%m-%d %H:%M:%S')
 
-ax.text(x1, y1+10, 'By',
-        fontsize = 20)
+
+#y1 = 19200
+#x1 = 5
+
+ax.plot(date,price)
+ax.scatter(now_date,now_price)
+ax.hlines(now_price*0.99, date[0],date[-1] )
+ax.hlines(now_price*1.01, date[0],date[-1] )
+
+#ax.text(x1, y1+10, 'By',fontsize = 20)
 
 ax.set(title = 'test')
 
 
 
 if __name__ == '__main__':
-	
+
 	plt.show()
+	#test()
